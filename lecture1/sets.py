a77e2e4131690bfd6c66b529b49c340252da9b65
nMDaas <natasha.daas@gmail.com>
2020-05-03 13:01:33 +0530

    dictionaries, functions, modules, sets

---
+++ lecture1/sets.py
@@ -0,0 +1,18 @@
+s = set()
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(4)
+s.add(5)
+print(s)
+
+#each elements in a set are unique if: 
+s = set()
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(3)
+s.add(5)
+print(s)
+
+#the set will print s = {1, 2, 3, 5}
