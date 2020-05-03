e3df1b0a7ce52a13f0b7763c7a8e01f519df375c
nMDaas <natasha.daas@gmail.com>
2020-05-03 17:41:29 +0530

    application 2

--- lecture1/application2.py
+++ lecture1/application2.py
@@ -6,13 +6,9 @@ app = Flask(__name__)
 def index():
   return"Hello, World!!"
 
-@app.route("/david")
-def david():
-    return("Hello, David!")
-
-@app.route("/maria")
-def maria():
-    return("Hello, Maria!")
+@app.route("/<string:name>")
+def hello(name):
+    return f"Hello {name}!"
 
 #run with:
 #export FLASK_APP=application1.py
