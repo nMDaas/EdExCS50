3757458d7c9719222498e05b1ef7b124a668aff6
nMDaas <natasha.daas@gmail.com>
2020-05-09 11:50:08 +0530

    Inserted data/rows and simple queries

--- lecture1/create.sql
+++ lecture1/create.sql
@@ -5,6 +5,26 @@ CREATE TABLE flights(
   duration INTEGER NOT NULL
 );
 
+INSERT INTO flights (origin, destination, duration) VALUES('New York', 'London', 415);
+INSERT INTO flights (origin, destination, duration) VALUES('Shanghai', 'Paris', 760);
+INSERT INTO flights (origin, destination, duration) VALUES('Istanbul', 'Tokyo', 700);
+INSERT INTO flights (origin, destination, duration) VALUES('New York', 'Paris', 435);
+INSERT INTO flights (origin, destination, duration) VALUES('Moscow', 'Paris', 245);
+INSERT INTO flights (origin, destination, duration) VALUES('Lima', 'New York', 455);
+
+#-----Queries-----
+#To see different relations of flights - \d
+
+#To see TABLE
+#SELECT * FROM flights;
+#SELECT origin, destination FROM flights;
+#SELECT * FROM flights WHERE id = 3;
+#SELECT * FROM flights WHERE origin = 'New York';
+#SELECT * FROM flights WHERE duration > 500;
+#SELECT * FROM flights WHERE destination = 'Paris' AND duration > 500;
+#SELECT * FROM flights WHERE destination = 'Paris' OR duration > 500;
+
+#-----Running-----
 # To connect to an existing database ( called mydb) on a db server that is already running
 # psql mydb natasha
 #exit
@@ -15,5 +35,3 @@ su superman
 exit
 pg_ctl -D /usr/local/var/postgres start
 pg_ctl -D /usr/local/var/postgres stop
-
-#To see Table - \d
